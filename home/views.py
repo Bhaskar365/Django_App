a8bc5e17711f217faa8ad7a807e87b18ff8f6863
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-10 17:34:23 +0530

    django message added

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    context = {
-        "ViewVar" : "This is a sample text from views.py"
-    }
+    # context = {
+    #     "ViewVar" : "This is a sample text from views.py"
+    # }
     return render(request, 'index.html')
     # return HttpResponse("This is home page")
 
@@ -25,5 +26,5 @@ def contact(request):
 
         contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
-
+    messages.success(request, "Your message has been sent!.")
     return render(request, 'contact.html')
